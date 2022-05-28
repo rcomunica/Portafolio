66a9daf42b7fd0781f06d2a127804eafc51b1da1
rcomunica <julir2772@gmail.com>
2022-05-28 11:40:24 -0500

    Modificacion de CSS por BUG

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 
 var TextNeon = document.getElementById('neonText')
-var Time = 9500
+var time = 2000
+
 
 function AparecerNeon(){
     if(TextNeon.style.display="none"){
-        setTimeout(function(){
+        settimeout(function(){
             TextNeon.style.transition="opacity 0.8s ease 1s";
             TextNeon.style.display="block"
-        }, Time)
+        }, time)
         
-        setTimeout(function(){
+        settimeout(function(){
             TextNeon.style.opacity="1"
-            console.log("En", Time, "se inicio el text neon y en", Time+500, "se termino")
-        }, Time+500)
+            console.log("En", time, "se inicio el text neon y en", time+500, "se termino")
+        }, time+500)
     }
     
 }
